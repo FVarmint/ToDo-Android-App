c53e6a52a2e5baa2f151d694d94f95292f349f11
fvarmint <chutanidishan@gmail.com>
2021-09-07 22:39:10 +0530

    commit

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.avjindersinghsekhon.minimaltodo.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultActivity;
+import com.example.avjindersinghsekhon.minimaltodo.R;
+import com.example.avjindersinghsekhon.minimaltodo.Settings.SettingsActivity;
+
+public class MainActivity extends AppDefaultActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
+    @Override
+    protected int contentViewLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    @NonNull
+    @Override
+    protected Fragment createInitialFragment() {
+        return MainFragment.newInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.preferences) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
